dcb4b593824610e02bc3db0c56759ec03c3d4c4a
OziV <oz.ziv8@gmail.com>
2022-09-11 16:18:25 +0300

    WIP: adding close btn for smallsidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,36 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleSidebar } from "../features/user/userSlice";
 
 function SmallSidebar() {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+
   return (
     <Wrapper>
-      <h1>SmallSidebar</h1>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">Nav Links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 }
