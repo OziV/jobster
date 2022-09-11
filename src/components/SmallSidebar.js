6dd65f12b76c2014414eec2a961c786b7611c664
OziV <oz.ziv8@gmail.com>
2022-09-11 16:31:08 +0300

    WIP: finish smallsidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleSidebar } from "../features/user/userSlice";
+import links from "../utils/links";
 
 function SmallSidebar() {
   const { isSidebarOpen } = useSelector((store) => store.user);
@@ -28,7 +29,24 @@ function SmallSidebar() {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">Nav Links</div>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => {
+                    return isActive ? "nav-link active" : "nav-link";
+                  }}
+                  key={id}
+                  onClick={toggle}
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Wrapper>
