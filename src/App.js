043664d73ed3878057c168ce10f94212dfd930e0
OziV <oz.ziv8@gmail.com>
2022-09-11 15:34:12 +0300

    WIP: adding ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Landing, Error, Dashboard, Register } from "./pages";
+import { Landing, Error, Dashboard, Register, ProtectedRoute } from "./pages";
 import {
   AddJob,
   AllJobs,
@@ -14,7 +14,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-job" element={<AddJob />} />
