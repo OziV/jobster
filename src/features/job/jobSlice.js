b0cba5e42fe8124d99b2792dc4c7e103b79f8977
OziV <oz.ziv8@gmail.com>
2022-09-12 18:56:08 +0300

    WIP: merge allJobs to main

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 import customFetch from "../../utils/axios";
 import { getUserFromLocalStorage } from "../../utils/localStorage";
-import { logoutUser } from "../user/userSlice";
+import { logoutUser } from "../user/userSlice ";
 
 const initialState = {
   isLoading: false,
